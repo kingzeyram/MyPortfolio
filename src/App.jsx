7cae79a7aabc4486ffa0b2cbc69920c129d85724
ramsey <lungahoramsey2005@gmail.com>
2025-06-27 18:48:21 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { MobileMenu } from './components/MobileMenu'
 import { Home } from './components/sections/Home'
 import { About } from './components/sections/About'
 import { Projects } from './components/sections/Projects'
-import { Contact } from './components/sections/Contacts'
+import { Contacts } from './components/sections/Contacts'
 
 
 function App() {
