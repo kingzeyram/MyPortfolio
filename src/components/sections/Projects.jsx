ff375f7f11b550baa365d848b0a410388b67cd1a
ramsey <lungahoramsey2005@gmail.com>
2025-08-14 15:19:55 +0300

    update

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -65,6 +65,24 @@ export const Projects = () => {
                             <a href="https://innovationweekapp.vercel.app/" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project →</a>
                         </div>
                     </div>
+                    {/* Fourth project */}
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59,130,246,0.2)] transition ">
+                        <h3 className="text-xl font-bold mb-2">Fashionova Web</h3>
+                        <p className="text-gray-400 mb-4">Our fashion store offers stylish, affordable, and high-quality clothing for all occasions. We specialize in trendy designs inspired by the latest fashion trends, ensuring our customers always look and feel their best. With a wide selection of apparel, footwear, and accessories, we cater to diverse styles and preferences, providing an enjoyable shopping experience both in-store and online</p>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {["Html", "Css", "js", "aos", "vercel"].map((tech, key) => (
+                                <span
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <a href="https://fashionova-web.vercel.app/" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project →</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </RevealOnScroll>
